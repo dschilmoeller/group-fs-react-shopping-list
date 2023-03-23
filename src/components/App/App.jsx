30b727ff138b7f1ee147535770c138c40e05362f
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-03-23 15:23:13 -0500

    conditional rendering completed.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -128,7 +128,7 @@ function App() {
                             <div class="groceryItem" key={item.id}>
                                 <p>{item.name}</p>
                                 <p>{item.qty} {item.unit}</p>
-                                <button class="buy" onClick={() => buyItem(item.id)}>Buy</button>
+                                {item.purchased ? null : <button class="buy" onClick={() => buyItem(item.id)}>Buy</button> }
                                 <button class="delete" onClick={() => deleteItem(item.id)}>Remove</button>
                             </div>
                         ))}
